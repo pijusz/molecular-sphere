f980624035a95a0bcf3d3788bbe3f0ddcf904b99
pijusz <pijusz@gmail.com>
2023-07-17 14:00:48 +0300

    new versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
+if (!process.env.VERCEL_ANALYTICS_ID) {
+  process.env.VERCEL_ANALYTICS_ID = process.env.PUBLIC_VERCEL_ANALYTICS_ID;
+}
+
 export default defineConfig({
   output: "hybrid",
   adapter: vercel({
-    // analytics: true,
+    analytics: true,
   }),
 });
