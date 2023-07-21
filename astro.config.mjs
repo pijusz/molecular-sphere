33238ff43dfec6c140ec06b8bd0bf4366d26503b
pijusz <pijusz@gmail.com>
2023-07-21 09:27:20 +0300

    redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
-if (!process.env.VERCEL_ANALYTICS_ID) {
-  process.env.VERCEL_ANALYTICS_ID = process.env.PUBLIC_VERCEL_ANALYTICS_ID;
-}
-
 export default defineConfig({
   output: "hybrid",
   adapter: vercel({
     analytics: true,
   }),
+  redirects: {
+    "/2": "/1",
+  },
 });
